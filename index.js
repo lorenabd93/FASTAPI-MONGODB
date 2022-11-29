e1b5b76a5c90fecac5ea2577eee1cb53d310d8b7
Lorena Barreto <lorenabd93@gmail.com>
2022-11-28 22:08:48 -0500

    banner_final

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+
+
+
 var app=new Vue({
     
     el: '#app',
     data: {
-        
+        message:'Carsold !!!',
         info:[]
     },
     mounted(){
         axios.get("http://127.0.0.1:8000/").then(respuesta=>this.info=respuesta.data)
     },
+
+
+
+
 });
+
+
 
